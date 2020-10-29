bde09aeb656ee521e0bca738f8420c0898b6f318
Devin Hinzo <devin.hinzo@gmail.com>
2020-10-29 16:22:57 -0400

    presented version

--- models/users.js
+++ models/users.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     artistName: { type: String, unique: true, required: true },
     realName: String,
     password: { type: String, required: true },
-    isComposer: Boolean,
-    isBoth: Boolean
+    // isComposer: Boolean,
+    // isBoth: Boolean
 })
 
 const User = mongoose.model('User', userSchema)
